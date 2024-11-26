b361ca1339124f2ecce7d59a3b5ec806d71f913f
BriceK <bricekauffman@gmail.com>
2024-11-26 13:07:39 -0800

    week 13 starter

---
+++ src/main/java/pet/parks/entity/GeoLocation.java
@@ -0,0 +1,22 @@
+package pet.parks.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+public class GeoLocation {
+	private BigDecimal latitude;
+	private BigDecimal longitude;
+	
+	public GeoLocation(GeoLocation geoLocation) {
+		this.latitude = geoLocation.latitude;
+		this.longitude = geoLocation.longitude;
+	}
+	
+
+}
